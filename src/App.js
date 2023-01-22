67b8bad6d33d247a655f54a8abda42519a884ddc
mukundamMayank <mayank.mukundam@students.iiit.ac.in>
2023-01-22 20:46:27 +0530

    deployed website

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter basename={window.location.pathname || ''}>
       <Routes>
         <Route path="/nxtWave" element={<Login/>}/>
-        <Route path="/nxtWave/home" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/nxtWave/addItem" element={<AddUser/>}/>
       </Routes>
       </BrowserRouter>
